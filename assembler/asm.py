614118c98a5a11702e2357da02a250595fc7968e
Jason Tatem <jattdb@gmail.com>
2020-05-14 15:58:15 -0400

    adding labels to assembler

--- assembler/asm.py
+++ assembler/asm.py
@@ -1,4 +1,5 @@
-label_sep = '::'
+label_define = '::'
+label_ref = '``'
 
 class Opcode(object):
     def __init__(self, op_name, op_number, length_condition, arg1_condition, arg2_condition):
@@ -63,7 +64,18 @@ class Instruction(object):
         self.arg1 = int(segments[2])
         self.arg2 = int(segments[3])
         if self.length > 0:
-            self.payload = [int(n) for n in segments[4:self.length + 4]]
+            self.payload = list()
+            for n in segments[4:self.length + 4]:
+                if n.startswith(label_ref):
+                    label_name = n.split(label_ref)[1].rstrip()
+                    try:
+                        label = [l for l in prog.labels if l.name == label_name][0]
+                    except:
+                        raise Exception('Reference to label {} encountered but label not found'.format(label_name))
+                    lr = LabelReference(label)
+                    self.payload.append(lr)
+                else:
+                    self.payload.append(int(n))
         if opcode.validate(self):
             pass
         else:
@@ -81,12 +93,62 @@ class Instruction(object):
         return b''.join([n.to_bytes(4, byteorder='little') for n in result])
 
 
+class Label(object):
+    def __init__(self, name):
+        self.name = name
+        self.value = None
+
+
+class LabelReference(object):
+    def __init__(self, label):
+        self.label = label
+
+
+class Program(object):
+    def __init__(self):
+        self.program = list()
+        self.labels = list()
+
+    def resolve_labels(self):
+        position = 1000000
+        # Pass 1 - set values of all labels
+        for step in self.program:
+            if isinstance(step, Label):
+                print('Label {} has value {}'.format(step.name, position))
+                step.value = position
+            else:
+                position += step.length + 1
+        # Pass 2 - update references
+        for step in self.program:
+            if isinstance(step, Instruction):
+                if step.length > 0:
+                    for i in range(0, step.length):
+                        if isinstance(step.payload[i], LabelReference):
+                            print('Found ref to label {}'.format(step.payload[i].label.name))
+                            step.payload[i] = step.payload[i].label.value
+
+
+    def out(self):
+        inst = [i for i in self.program if isinstance(i, Instruction)]
+        return inst
+
+    
+
+
+
 def read_asm(file_name):
     with open(file_name, 'r') as f:
         data = f.read()
         lines = [l.split('#')[0] for l in data.split('\n')]
-        instructions = [Instruction(line) for line in lines if line != '']
-    return instructions
+        # find all labels pass
+        prog.labels = [Label(line.split(label_define)[1].rstrip()) for line in lines if line.startswith(label_define)]
+        for line in lines:
+            if line.startswith(label_define):
+                l = [label for label in prog.labels if label.name == line.split(label_define)[1].rstrip()][0]
+                prog.program.append(l)
+            else:
+                i = Instruction(line)
+                prog.program.append(i)
 
 
 def write_bin(file_name, instructions):
@@ -99,5 +161,10 @@ if __name__=='__main__':
     parser.add_argument('-i', default='./test.asm')
     parser.add_argument('-o', default='../test.bin')
     args = parser.parse_args()
-    inst = read_asm(args.i)
+    #inst = read_asm(args.i)
+    #write_bin(args.o, inst)
+    prog = Program()
+    read_asm(args.i)
+    prog.resolve_labels()
+    inst = prog.out()
     write_bin(args.o, inst)
