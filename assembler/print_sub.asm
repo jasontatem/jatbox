1f8918ef171b6d52e095ff5fed5409d2b27461a6
Jason Tatem <jattdb@gmail.com>
2020-05-14 08:00:51 -0400

    print sub work

--- assembler/print_sub.asm
+++ assembler/print_sub.asm
@@ -1,2 +1,22 @@
-jump 1 0 0 xxxxx
-halt 0 0 0
+store 2 0 0 83 5000000 # 1000000
+store 2 0 0 85 5000001 # 1000003
+store 2 0 0 67 5000002 # 1000006
+store 2 0 0 75 5000003 # 1000009
+store 2 0 0 32 5000004 # 1000012
+store 2 0 0 73 5000005 # 1000015
+store 2 0 0 84 5000006 # 1000018
+store 2 0 0 10 5000007 # 1000021
+store 2 0 0 5000000 999980 # 1000024
+store 2 0 0 8 999981 # 1000027
+jump 1 0 0 1000033 # 1000030
+halt 0 0 0 # 1000032
+copy 2 0 0 999980 999000 # read arg1 (start addr) into BIOS working mem 1000033
+add 3 0 0 999980 999981 999001 # add arg1 and arg2, store result in BIOS working mem 1000036
+copy 2 1 0 999000 999002 # copy value pointed to by 999000 into 999002 1000040
+add 3 1 0 999002 16777216 999002 # add data_send bit to value in 999002 1000043
+copy 2 0 0 999002 16000000 # copy data to serial terminal output location 1000047
+add 3 1 0 999000 1 999000 # increment counter 1000050
+compare 2 1 1 999000 999001 # compare counter to target value 1000054
+branch 2 0 0 3 1000062 # branch to exit from routine 1000057
+goto 1 0 0 1000040 # back to 'copy 2 1 0 999000 999002' 1000060
+return 0 0 0 # 1000062
