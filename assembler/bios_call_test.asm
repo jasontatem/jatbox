5fd23eb8d347bc5f84e2fdc72065c30bdf3b6e92
Jason Tatem <jattdb@gmail.com>
2020-05-18 22:54:28 -0400

    testing new jump/branch stuff

--- assembler/bios_call_test.asm
+++ assembler/bios_call_test.asm
@@ -1,11 +1,11 @@
 store 2 0 0 0 $BIOS_ARG0 # Set which function to call (0)
 store 2 0 0 ``STRING_DATA $BIOS_ARG1 # Start of string data
 store 2 0 0 8 $BIOS_ARG2 # Length of string data
-jump 1 0 0 $BIOS_DISPATCHER_ENTRY
+jump 2 0 0 $BIOS_DISPATCHER_ENTRY 0
 store 2 0 0 1 $BIOS_ARG0
 store 2 0 0 ``PACKED_DATA $BIOS_ARG1
 store 2 0 0 2 $BIOS_ARG2
-jump 1 0 0 $BIOS_DISPATCHER_ENTRY
+jump 2 0 0 $BIOS_DISPATCHER_ENTRY 0
 halt 0 0 0
 ::STRING_DATA
 =&SUCK IT~n
